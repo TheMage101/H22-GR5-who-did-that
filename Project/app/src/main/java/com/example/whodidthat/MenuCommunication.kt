369d407a79388fa6a7e29231df0ec211e86bc303
MarinN <nicolasmarin10123@gmail.com>
2022-03-25 14:38:51 -0400

    Added functionality to the buttons in Main Menu
    Most of the buttons in the main menu now bring you to their respective activities

--- Project/app/src/main/java/com/example/whodidthat/MenuCommunication.kt
+++ Project/app/src/main/java/com/example/whodidthat/MenuCommunication.kt
@@ -2,10 +2,13 @@ package com.example.whodidthat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class MenuCommunication : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_communication)
+        val messages = findViewById<RecyclerView>(R.id.messages)
+        messages.add
     }
 }
