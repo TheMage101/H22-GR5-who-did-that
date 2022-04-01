b85d7dee2a022eb37a32d17b8996faa1ca957f43
MarinN <nicolasmarin10123@gmail.com>
2022-04-01 14:50:36 -0400

    Big Update
    fixed all the bugs

--- Project/app/src/main/java/com/example/whodidthat/MainActivity.kt
+++ Project/app/src/main/java/com/example/whodidthat/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.example.whodidthat
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import com.example.whodidthat.databinding.ActivityMainBinding
+import androidx.appcompat.app.AppCompatActivity
 
-class MenuPrincipal : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,32 +16,31 @@ class MenuPrincipal : AppCompatActivity() {
         Personne.savePerson(peep)
 
         //le bouton change l'activité à MenuCommunication
-        val communicationButton = findViewById<Button>(R.id.button1)
+        val communicationButton = findViewById<Button>(R.id.button)
         communicationButton.setOnClickListener {
-            val intent = Intent(this@MenuPrincipal, MenuCommunication::class.java)
+            val intent = Intent(this@MainActivity, MenuCommunication::class.java)
             startActivity(intent)
         }
 
         //le bouton change l'activité à MenuNotes
         val notesButton = findViewById<Button>(R.id.button2)
         notesButton.setOnClickListener {
-            val intent = Intent(this@MenuPrincipal, MenuNotes::class.java)
+            val intent = Intent(this@MainActivity, MenuNotes::class.java)
             startActivity(intent)
         }
 
         //le bouton change l'activité à MenuCalendrier
         val calendrierButton = findViewById<Button>(R.id.button3)
         calendrierButton.setOnClickListener {
-            val intent = Intent(this@MenuPrincipal, MenuCalendrier::class.java)
+            val intent = Intent(this@MainActivity, MenuCalendrier::class.java)
             startActivity(intent)
         }
 
         //le bouton change l'activité à MenuReglages
         val reglagesButton = findViewById<Button>(R.id.button4)
         notesButton.setOnClickListener {
-            val intent = Intent(this@MenuPrincipal, MenuReglages::class.java)
+            val intent = Intent(this@MainActivity, MenuReglages::class.java)
             startActivity(intent)
         }
-
     }
 }
--- Project/app/src/main/java/com/example/whodidthat/Personne.kt
+++ Project/app/src/main/java/com/example/whodidthat/Personne.kt
@@ -1,4 +1,32 @@
 package com.example.whodidthat
 
-class Personne {
+val peoples = ArrayList<Personne>()
+var currentUser: Int = 0
+
+class Personne(var name: String, var age: String) {
+
+    lateinit var gender: String
+    lateinit var description: String
+    var notes: String = ""
+
+    /**
+     * Utiliser pour sauvegarder un nouvelle utilisateur
+     * @param peep est la personne a ajoutÃ©
+     */
+
+    companion object {
+
+        fun savePerson(peep: Personne) {
+            peoples.add(peep)
+        }
+
+
+        fun getCurrentUser(): Personne {
+            return peoples[currentUser]
+        }
+
+        fun saveNotes(notes: String) {
+            getCurrentUser().notes = notes
+        }
+    }
 }
