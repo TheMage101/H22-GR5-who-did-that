e7726dd18d75c6ed4f19c6600cc9ceb0111bdbe8
Èvan Moreau <evan.moreau@videotron.ca>
2022-04-01 15:50:14 -0400

    Added Principal Classes and Graphics Interfaces
    -added MenuCalendrier activity
    
    -added MenuCreationPersonnage activity
    
    -added MenuCommunication activity
    
    -added MenuReglages activity
    
    -added MenuProfil activity
    
    -added MenuNotes activity
    
    -added Personne class

--- Project/app/src/main/java/com/example/whodidthat/MainActivity.kt
+++ Project/app/src/main/java/com/example/whodidthat/MainActivity.kt
@@ -17,28 +17,28 @@ class MainActivity : AppCompatActivity() {
         Personne.savePerson(peep)
 
         //le bouton change l'activité à MenuCommunication
-        val communicationButton = findViewById<Button>(R.id.button)
+        val communicationButton = findViewById<Button>(R.id.button1)
         communicationButton.setOnClickListener {
             val intent = Intent(this@MainActivity, MenuCommunication::class.java)
             startActivity(intent)
         }
 
         //le bouton change l'activité à MenuNotes
-        val notesButton = findViewById<Button>(R.id.button3)
+        val notesButton = findViewById<Button>(R.id.button2)
         notesButton.setOnClickListener {
             val intent = Intent(this@MainActivity, MenuNotes::class.java)
             startActivity(intent)
         }
 
         //le bouton change l'activité à MenuCalendrier
-        val calendrierButton = findViewById<Button>(R.id.button5)
+        val calendrierButton = findViewById<Button>(R.id.button3)
         calendrierButton.setOnClickListener {
             val intent = Intent(this@MainActivity, MenuCalendrier::class.java)
             startActivity(intent)
         }
 
         //le bouton change l'activité à MenuReglages
-        val reglagesButton = findViewById<Button>(R.id.button6)
+        val reglagesButton = findViewById<Button>(R.id.button4)
         reglagesButton.setOnClickListener {
             val intent = Intent(this@MainActivity, MenuReglages::class.java)
             startActivity(intent)
