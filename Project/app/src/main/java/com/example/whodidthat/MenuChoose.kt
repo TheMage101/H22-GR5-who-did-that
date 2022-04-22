baf235949bc04a94cfbec4682693182abf3905a5
Èvan Moreau <evan.moreau@videotron.ca>
2022-04-22 14:02:26 -0400

    nothing importanf

--- Project/app/src/main/java/com/example/whodidthat/MenuChoose.kt
+++ Project/app/src/main/java/com/example/whodidthat/MenuChoose.kt
@@ -11,22 +11,22 @@ class MenuChoose : AppCompatActivity() {
         setContentView(R.layout.activity_menu_person_choosing)
 
 
-//        val addPersonButton = findViewById<Button>(R.id.ButtonAddPerson)
-//        addPersonButton.setOnClickListener {
-//            val intent = Intent(this@MenuChoose, MenuCreationPersonne::class.java)
-//            startActivity(intent)
-//        }
-//
-//        val person1bouton = findViewById<Button>(R.id.person1Button)
-//        person1bouton.setOnClickListener {
-//            val intent = Intent(this@MenuChoose, MainActivity::class.java)
-//            startActivity(intent)
-//        }
-//
-//        val person2bouton = findViewById<Button>(R.id.person2Button)
-//        person2bouton.setOnClickListener {
-//            val intent = Intent(this@MenuChoose, MainActivity::class.java)
-//            startActivity(intent)
-//        }
+        val addPersonButton = findViewById<Button>(R.id.ButtonAddPerson)
+        addPersonButton.setOnClickListener {
+            val intent = Intent(this@MenuChoose, MenuCreationPersonne::class.java)
+            startActivity(intent)
+        }
+
+        val person1bouton = findViewById<Button>(R.id.person1Button)
+        person1bouton.setOnClickListener {
+            val intent = Intent(this@MenuChoose, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        val person2bouton = findViewById<Button>(R.id.person2Button)
+        person2bouton.setOnClickListener {
+            val intent = Intent(this@MenuChoose, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
